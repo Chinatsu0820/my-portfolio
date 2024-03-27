da189bb27ae454f132a4481c799484f0f23571bd
Chinatsu0820 <01417gohan@gmail.com>
2024-03-27 08:42:45 -0700

    make button component

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/components/atoms/Button.jsx",
+    "./src/components/molecules/Menu.jsx",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
