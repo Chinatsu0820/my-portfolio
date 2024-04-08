536bafb02b99aa812679289da8c9ecaf4109ba68
Chinatsu0820 <01417gohan@gmail.com>
2024-04-08 15:29:23 -0700

    edit history&top

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Footer from './components/sections/Footer';
 import History from './components/pages/History';
 import Can from './components/pages/Can';
 import Contact from './components/pages/Contact';
+import Welcomeimg from './assets/imgs/akazukinchan.png';
 import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -12,6 +13,8 @@ function App() {
     <div className='App'>
       <Header />
       <div className='Home'>
+        <h1> Welcome to my website!</h1>
+        <img className='home-img' src={Welcomeimg} alt='This is a picture for toppage'/>
       <Routes>
         <Route path='/History' element={<History />} />
         <Route path='/Can' element={<Can />} />
